102a82f057065520ced8e1bab7b6366bb9af18fc
Mihail Denisenko <i@mihail-denisenko.ru>
2024-10-04 15:58:09 +0300

    com

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const mult = (a, b) => a + b;
 import html from './index.html'
 // import "./index.css";
-import './index.scss'
+
 // import Swiper JS
 import Swiper from 'swiper';
 // import Swiper styles
 import 'swiper/css';
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+import './index.scss'
+
 
 const swiper = new Swiper(".swiper-menu", {
   direction: "horizontal",
