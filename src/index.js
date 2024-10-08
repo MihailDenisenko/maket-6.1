31f1a73112eb2ad990e1f3ffb5cca41a8a042546
Mihail Denisenko <i@mihail-denisenko.ru>
2024-10-08 14:31:42 +0300

    commmit-08.10.2024-14:30

--- src/index.js
+++ src/index.js
@@ -6,17 +6,74 @@ import html from './index.html'
 import Swiper from 'swiper';
 // import Swiper styles
 import 'swiper/css';
+
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+
 import './index.scss'
-// import './scripts/nav_menu-swiper'
 
 
 const swiper = new Swiper(".swiper-menu", {
   direction: "horizontal",
   slidesPerView: "auto",
-  spaceBetween: 10,
+  spaceBetween: 50,
   freeMode: true,
 });
 
-import {consol} from "./scripts/nav_menu-swiper.js"
+
+
+import "swiper/bundle";
+
+// Функция проверки ширины экнрана 
+window.addEventListener('DOMContentLoaded', () => {
+  var wid = document.querySelector('.body').clientWidth
+  
+//  Проверка (320-767 => swiper)
+  if (wid > 320 && wid < 768) {
+    console.log(`Новые размеры: ${wid}`);
+    var swiper_brand = new Swiper(".swiper-brands", {
+      slidesPerView: "auto",
+      freeMode: true,
+      spaceBetween: 50,
+
+      pagination: {
+        el: ".swiper-pagination",
+        type: "bullets",
+        // dynamicBullets: true,
+        clickable: true,
+      },
+    }); 
+    
+    var swiper_tech = new Swiper(".swiper-tehnics", {
+      slidesPerView: "auto",
+      freeMode: true,
+      spaceBetween: 50,
+
+      pagination: {
+        el: ".swiper-pagination",
+        type: "bullets",
+        // dynamicBullets: true,
+        clickable: true,
+      },
+    }); 
+
+    var swiper_price = new Swiper(".swiper-price", {
+      slidesPerView: "auto",
+      freeMode: true,
+      spaceBetween: 50,
+
+      pagination: {
+        el: ".swiper-pagination",
+        type: "bullets",
+        // dynamicBullets: true,
+        clickable: true,
+      },
+    }); 
+
+
+
+
+
+  }
+});
+
