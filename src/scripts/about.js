e915b01a65aa87467e588af807c5fadd108631c9
Mihail Denisenko <i@mihail-denisenko.ru>
2024-10-10 08:21:14 +0300

    last-commit-10.10.2024-8:20

--- src/scripts/about.js
+++ src/scripts/about.js
@@ -1,3 +1,4 @@
+let numArray = [];
 const about_img = document.querySelector(".about__img");
 const about_p = document.querySelector(".about_p");
 const show__brands = document.querySelector(".show__brands");
@@ -8,6 +9,8 @@ const tehnics__ul = document.querySelector(".tehnics__ul");
 
     
 about_img.addEventListener("click", function () {
+  numArray.push(1);
+  console.log(numArray.length)
   if (about_img.classList.contains("about__img")) {
     about_p.classList.add("about_p-more");
     about_img.classList.remove("about__img");
